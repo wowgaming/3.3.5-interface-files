d0339b17b0221db76e6acd2dc2915d224a5b62ca
Yehonal <yehonal.azeroth@gmail.com>
2020-12-27 20:54:41 +0100

    Imported all files

---
+++ Blizzard_GMChatUI/Blizzard_GMChatUI.lua
@@ -0,0 +1,181 @@
+local ListOfGMs = {};
+
+function GMChatFrame_IsGM(playerName)
+	return ListOfGMs[strlower(playerName)];
+end
+
+function GMChatFrame_OnLoad(self)
+	local name = self:GetName();
+	for index, value in pairs(CHAT_FRAME_TEXTURES) do
+		local object = _G[name..value];
+		local objectType = object:GetObjectType();
+		if ( objectType == "Button" ) then
+			object:GetNormalTexture():SetVertexColor(0, 0, 0);
+			object:GetHighlightTexture():SetVertexColor(0, 0, 0);
+			object:GetPushedTexture():SetVertexColor(0, 0, 0);
+		elseif ( objectType == "Texture" ) then
+			_G[name..value]:SetVertexColor(0,0,0);
+		else
+			--error("Unhandled object type");
+		end
+		object:SetAlpha(0.4);
+	end
+	
+	self:RegisterEvent("CHAT_MSG_WHISPER");
+	self:RegisterEvent("CHAT_MSG_WHISPER_INFORM");
+	self:RegisterEvent("UPDATE_CHAT_COLOR");
+	self:RegisterEvent("UPDATE_CHAT_WINDOWS");
+	self.flashTimer = 0;
+	self.lastGM = {};
+	
+	GMChatOpenLog:Enable();
+	
+	self:SetClampRectInsets(-35, 0, 30, 0);
+	
+	self:SetFont(DEFAULT_CHAT_FRAME:GetFont());
+end
+
+function GMChatFrame_OnEvent(self, event, ...)
+	local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 = ...;
+	if ( event == "CHAT_MSG_WHISPER" and arg6 == "GM" ) then
+		local info = ChatTypeInfo["WHISPER"];
+		
+		local pflag = "|TInterface\\ChatFrame\\UI-ChatIcon-Blizz.blp:0:2:0:-3|t ";
+		
+		-- Search for icon links and replace them with texture links.
+		local term;
+		for tag in string.gmatch(arg1, "%b{}") do
+			term = strlower(string.gsub(tag, "[{}]", ""));
+			if ( ICON_TAG_LIST[term] and ICON_LIST[ICON_TAG_LIST[term]] ) then
+				arg1 = string.gsub(arg1, tag, ICON_LIST[ICON_TAG_LIST[term]] .. "0|t");
+			end
+		end
+		
+		local body = format(CHAT_WHISPER_GET, pflag.."|HplayerGM:"..arg2..":"..arg11.."|h".."["..arg2.."]".."|h")..arg1;
+		
+		ListOfGMs[strlower(arg2)] = true;
+		self:AddMessage(body, info.r, info.g, info.b, info.id);
+		
+		if ( self.lastGMForCVar ~= arg2 and GMChatFrame:IsShown() ) then
+			SetCVar("lastTalkedToGM", arg2);
+		end
+		self.lastGMForCVar = arg2;
+		
+		if ( not GMChatFrame:IsShown() ) then
+			GMChatStatusFrame:Show();
+			GMChatStatusFrame_Pulse();
+			table.insert(self.lastGM,arg2);
+			PlaySound("GM_ChatWarning");
+			
+			DEFAULT_CHAT_FRAME:AddMessage(pflag.."|HGMChat|h["..GM_CHAT_STATUS_READY_DESCRIPTION.."]|h", info.r, info.g, info.b, info.id);
+			DEFAULT_CHAT_FRAME:SetHyperlinksEnabled(true);
+			DEFAULT_CHAT_FRAME.overrideHyperlinksEnabled = true;
+			SetButtonPulse(HelpMicroButton, 3600, 1.0);
+			SetButtonPulse(GMChatOpenLog, 3600, 1.0);
+		else
+			ChatEdit_SetLastTellTarget(arg2);
+		end
+	elseif ( event == "CHAT_MSG_WHISPER_INFORM" and GMChatFrame_IsGM(arg2) ) then
+		local info = ChatTypeInfo["WHISPER_INFORM"];
+		
+		local pflag = "|TInterface\\ChatFrame\\UI-ChatIcon-Blizz.blp:0:2:0:-3|t ";
+		
+		-- Search for icon links and replace them with texture links.
+		local term;
+		for tag in string.gmatch(arg1, "%b{}") do
+			term = strlower(string.gsub(tag, "[{}]", ""));
+			if ( ICON_TAG_LIST[term] and ICON_LIST[ICON_TAG_LIST[term]] ) then
+				arg1 = string.gsub(arg1, tag, ICON_LIST[ICON_TAG_LIST[term]] .. "0|t");
+			end
+		end
+		
+		local body = format(CHAT_WHISPER_INFORM_GET, pflag.."|HplayerGM:"..arg2..":"..arg11.."|h".."["..arg2.."]".."|h")..arg1;
+		
+		self:AddMessage(body, info.r, info.g, info.b, info.id);
+	elseif ( event == "UPDATE_CHAT_COLOR" ) then
+		local arg1, arg2, arg3, arg4 = ...
+		local info = ChatTypeInfo[strupper(arg1)];
+		if ( info ) then
+			info.r = arg2;
+			info.g = arg3;
+			info.b = arg4;
+			self:UpdateColorByID(info.id, info.r, info.g, info.b);
+
+			if ( strupper(arg1) == "WHISPER" ) then
+				info = ChatTypeInfo["REPLY"];
+				if ( info ) then
+					info.r = arg2;
+					info.g = arg3;
+					info.b = arg4;
+					self:UpdateColorByID(info.id, info.r, info.g, info.b);
+				end
+			end
+		end
+	elseif ( event == "UPDATE_CHAT_WINDOWS" ) then
+		local _, fontSize= FCF_GetChatWindowInfo(1);
+		if ( fontSize > 0 ) then
+			local fontFile, unused, fontFlags = DEFAULT_CHAT_FRAME:GetFont();
+			self:SetFont(fontFile, fontSize, fontFlags);
+		end
+	end
+end
+
+function GMChatFrame_OnShow(self)
+	GMChatStatusFrame:Hide();
+	GMChatOpenLog:Disable();
+	for _,gmName in ipairs(self.lastGM) do
+		ChatEdit_SetLastTellTarget(gmName);
+	end
+	table.wipe(self.lastGM);
+	if ( self.lastGMForCVar ) then
+		SetCVar("lastTalkedToGM", self.lastGMForCVar);
+	end
+	
+	SetButtonPulse(HelpMicroButton, 0, 1);	--Stop the buttons from pulsing.
+	SetButtonPulse(GMChatOpenLog, 0, 1);
+	
+	self:SetScript("OnUpdate", GMChatFrame_OnUpdate);
+end
+
+function GMChatFrame_OnHide(self)
+	GMChatOpenLog:Enable();
+	SetCVar("lastTalkedToGM", "");
+end
+
+function GMChatFrame_OnUpdate(self, elapsed)
+	if ( DEFAULT_CHAT_FRAME.isUninteractable ) then
+		DEFAULT_CHAT_FRAME:SetHyperlinksEnabled(false);
+	end
+	DEFAULT_CHAT_FRAME.overrideHyperlinksEnabled = false;
+	
+	self:SetScript("OnUpdate", nil);
+end
+
+function GMChatFrame_Show()
+	GMChatFrame:Show();
+end
+
+function GMChatFrame_Close()
+	GMChatFrame:Hide();
+end
+
+function GMChatStatusFrame_OnClick()
+	GMChatFrame_Show();
+end
+
+local function GMChatStatusFrame_PulseFunc(self, elapsed)
+	return abs(sin(elapsed*180*450--[[<--Number of times to pulse here]]));
+end
+
+local GMChatStatusFrame_PulseTable = {
+	totalTime = 900,
+	updateFunc = "SetAlpha",
+	getPosFunc = GMChatStatusFrame_PulseFunc,
+}
+
+function GMChatStatusFrame_Pulse()
+	local pulse = GMChatStatusFramePulse;
+	pulse:Show();
+	pulse:SetAlpha(0);
+	SetUpAnimation(pulse, GMChatStatusFrame_PulseTable, pulse.Hide);
+end
---
+++ Blizzard_InspectUI/InspectPVPFrame.lua
@@ -0,0 +1,176 @@
+
+function InspectPVPFrame_OnLoad(self)
+	InspectPVPFrameLine1:SetAlpha(0.3);
+	InspectPVPHonorKillsLabel:SetVertexColor(0.6, 0.6, 0.6);
+	InspectPVPHonorHonorLabel:SetVertexColor(0.6, 0.6, 0.6);
+	InspectPVPHonorTodayLabel:SetVertexColor(0.6, 0.6, 0.6);
+	InspectPVPHonorYesterdayLabel:SetVertexColor(0.6, 0.6, 0.6);
+	InspectPVPHonorLifetimeLabel:SetVertexColor(0.6, 0.6, 0.6);
+
+	self:RegisterEvent("INSPECT_HONOR_UPDATE");
+end
+
+function InspectPVPFrame_OnEvent(self, event, ...)
+	if ( event == "INSPECT_HONOR_UPDATE" ) then
+		InspectPVPFrame_Update();
+	end
+end
+
+function InspectPVPFrame_OnShow()
+	InspectPVPFrame_Update();
+	if ( not HasInspectHonorData() ) then
+		RequestInspectHonorData();
+	else
+		InspectPVPFrame_Update();
+	end
+end
+
+function InspectPVPFrame_SetFaction()
+	local factionGroup = UnitFactionGroup("player");
+	if ( factionGroup ) then
+		InspectPVPFrameHonorIcon:SetTexture("Interface\\TargetingFrame\\UI-PVP-"..factionGroup);
+		InspectPVPFrameHonorIcon:Show();
+	end
+end
+
+function InspectPVPFrame_Update()
+	for i=1, MAX_ARENA_TEAMS do
+		GetInspectArenaTeamData(i);
+	end	
+	InspectPVPFrame_SetFaction();
+	InspectPVPHonor_Update();
+	InspectPVPTeam_Update();
+end
+
+function InspectPVPTeam_Update()
+	-- Display Elements
+	local button, buttonName, highlight, data, standard, emblem, border;
+	-- Data Elements
+	local teamName, teamSize, teamRating, teamPlayed, teamWins, teamLoss, playerPlayed,  playerRating, playerPlayedPct, teamRank;
+	local background = {};
+	local borderColor = {};
+	local emblemColor = {};
+	local ARENA_TEAMS = {};
+	ARENA_TEAMS[1] = {size = 2};
+	ARENA_TEAMS[2] = {size = 3};
+	ARENA_TEAMS[3] = {size = 5};
+
+	-- Sort teams by size
+
+	local buttonIndex = 0;
+	for index, value in pairs(ARENA_TEAMS) do
+		for i=1, MAX_ARENA_TEAMS do
+			teamName, teamSize = GetInspectArenaTeamData(i);
+			if ( value.size == teamSize ) then
+				value.index = i;
+			end
+		end
+	end
+
+	-- fill out data
+	for index, value in pairs(ARENA_TEAMS) do
+		if ( value.index ) then
+			buttonIndex = buttonIndex + 1;
+			-- Pull Values
+			teamName, teamSize, teamRating, teamPlayed, teamWins,  playerPlayed, playerRating, background.r, background.g, background.b, emblem, emblemColor.r, emblemColor.g, emblemColor.b, border, borderColor.r, borderColor.g, borderColor.b = GetInspectArenaTeamData(value.index);
+			teamLoss = teamPlayed - teamWins;
+			if ( teamPlayed ~= 0 ) then
+				playerPlayedPct =  floor( ( playerPlayed / teamPlayed ) * 100 );		
+			else
+				playerPlayedPct =  floor( ( playerPlayed / 1 ) * 100 );
+			end
+
+			-- Set button elements to variables 
+			button = _G["InspectPVPTeam"..buttonIndex];
+			buttonName = "InspectPVPTeam"..buttonIndex;
+			data = buttonName.."Data";
+			standard = buttonName.."Standard";
+
+			button:SetID(value.index);
+
+			-- Populate Data
+			_G[data.."TypeLabel"]:SetText(ARENA_THIS_SEASON);
+			_G[data.."Name"]:SetText(teamName);
+			_G[data.."Rating"]:SetText(teamRating);
+			_G[data.."Games"]:SetText(teamPlayed);
+			_G[data.."Wins"]:SetText(teamWins);
+			_G[data.."Loss"]:SetText(teamLoss);
+			
+			_G[data.."Played"]:SetText(playerRating);
+			_G[data.."Played"]:SetVertexColor(1.0, 1.0, 1.0);
+			_G[data.."PlayedLabel"]:SetText(RATING);
+
+			-- Set TeamSize Banner
+			_G[standard.."Banner"]:SetTexture("Interface\\PVPFrame\\PVP-Banner-"..teamSize);
+			_G[standard.."Banner"]:SetVertexColor(background.r, background.g, background.b);
+			_G[standard.."Border"]:SetVertexColor(borderColor.r, borderColor.g, borderColor.b);
+			_G[standard.."Emblem"]:SetVertexColor(emblemColor.r, emblemColor.g, emblemColor.b);
+			if ( border ~= -1 ) then
+				_G[standard.."Border"]:SetTexture("Interface\\PVPFrame\\PVP-Banner-"..teamSize.."-Border-"..border);
+			end
+			if ( emblem ~= -1 ) then
+				_G[standard.."Emblem"]:SetTexture("Interface\\PVPFrame\\Icons\\PVP-Banner-Emblem-"..emblem);
+			end
+
+			-- Set visual elements
+			_G[data]:Show();
+			button:SetAlpha(1);
+			_G[buttonName.."Highlight"]:SetAlpha(1);
+			_G[buttonName.."Highlight"]:SetBackdropBorderColor(1.0, 0.82, 0);
+			_G[standard]:SetAlpha(1);
+			_G[standard.."Border"]:Show();
+			_G[standard.."Emblem"]:Show();
+			_G[buttonName.."Background"]:SetVertexColor(0, 0, 0);
+			_G[buttonName.."Background"]:SetAlpha(1);
+			_G[buttonName.."TeamType"]:Hide();
+			
+		end
+	end
+
+	-- show unused teams
+	for index, value in pairs(ARENA_TEAMS) do
+		if ( not value.index ) then
+			-- Set button elements to variables 
+			buttonIndex = buttonIndex + 1;
+			button = _G["InspectPVPTeam"..buttonIndex];
+			buttonName = "InspectPVPTeam"..buttonIndex;
+			data = buttonName.."Data";
+
+			-- Set standard type
+			_G[buttonName.."StandardBanner"]:SetTexture("Interface\\PVPFrame\\PVP-Banner-"..value.size);
+
+			-- Hide or Show items
+			button:SetAlpha(0.4);
+			_G[data]:Hide();
+			_G[buttonName.."Background"]:SetVertexColor(0, 0, 0);
+			_G[buttonName.."Standard"]:SetAlpha(0.1);
+			_G[buttonName.."StandardBorder"]:Hide();
+			_G[buttonName.."StandardEmblem"]:Hide();
+			_G[buttonName.."TeamType"]:SetFormattedText(PVP_TEAMSIZE, value.size, value.size);
+			_G[buttonName.."TeamType"]:Show();
+		end
+	end
+end
+
+-- PVP Honor Data
+function InspectPVPHonor_Update()
+	local todayHK, todayHonor, yesterdayHK, yesterdayHonor, lifetimeHK, lifetimeRank = GetInspectHonorData();
+	
+	-- Yesterday's values
+	InspectPVPHonorYesterdayKills:SetText(yesterdayHK);
+	InspectPVPHonorYesterdayHonor:SetText(yesterdayHonor);
+	
+	-- Lifetime values
+	InspectPVPHonorLifetimeKills:SetText(lifetimeHK);
+	InspectPVPFrameHonorPoints:SetText("");
+	InspectPVPFrameArenaPoints:SetText("");
+
+	-- Hide Point Values
+	InspectPVPFrameHonorPoints:Hide();	
+	InspectPVPFrameArenaPoints:Hide();
+	
+	-- This session's values
+	InspectPVPHonorTodayKills:SetText(todayHK);
+	InspectPVPHonorTodayHonor:SetText(todayHonor);
+	InspectPVPHonorTodayHonor:SetHeight(14);
+end
